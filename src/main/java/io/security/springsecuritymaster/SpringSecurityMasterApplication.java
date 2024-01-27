0c8ac898a918ec1c0e836fc5acd1af8f486a1c57
onjsdnjs <onjsdnjs@gmail.com>
2024-01-27 22:47:42 +0900

    Initial commit

---
+++ src/main/java/io/security/springsecuritymaster/SpringSecurityMasterApplication.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
