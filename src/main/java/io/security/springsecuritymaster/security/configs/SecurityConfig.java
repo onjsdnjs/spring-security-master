72af7b4b5862a78ba7a9da3dac9edab4a9dc4bd1
onjsdnjs <onjsdnjs@gmail.com>
2024-01-28 22:31:45 +0900

    프로젝트 생성 및 기본구성

--- src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +25,7 @@ public class SecurityConfig {
 
                 //FIXME
                 .formLogin(form -> form.loginPage("/login").permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
