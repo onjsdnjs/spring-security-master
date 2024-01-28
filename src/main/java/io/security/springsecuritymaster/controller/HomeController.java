153fd3ef49667272d2e0f635c7b00229d4e2a029
onjsdnjs <onjsdnjs@gmail.com>
2024-01-28 23:46:30 +0900

    프로젝트 생성 및 기본구성

--- src/main/java/io/security/springsecuritymaster/controller/HomeController.java
+++ src/main/java/io/security/springsecuritymaster/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
+public class HomeController {
 	@GetMapping(value="/")
 	public String dashboard() {
 		return "/dashboard";
