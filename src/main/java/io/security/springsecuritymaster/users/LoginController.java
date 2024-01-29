9fe663cb0dc79e5967fddd039748b8cbcc9cac64
onjsdnjs <onjsdnjs@gmail.com>
2024-01-29 15:31:21 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/LoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/LoginController.java
@@ -1,5 +1,10 @@
 package io.security.springsecuritymaster.users;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,5 +18,15 @@ public class LoginController {
     @GetMapping(value="/signup")
     public String signup() {
         return "login/signup";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
