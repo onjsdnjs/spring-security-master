0cbc5c4a67197fd082f859c3226c54d2baccb3a0
onjsdnjs <onjsdnjs@gmail.com>
2024-01-29 15:05:22 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/security/provider/FormAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package io.security.springsecuritymaster.security.provider;
+
+import io.security.springsecuritymaster.domain.dto.AccountContext;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FormAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String loginId = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        AccountContext accountContext = (AccountContext)userDetailsService.loadUserByUsername(loginId);
+
+        if (!passwordEncoder.matches(password, accountContext.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(UsernamePasswordAuthenticationToken.class);;
+    }
+}
