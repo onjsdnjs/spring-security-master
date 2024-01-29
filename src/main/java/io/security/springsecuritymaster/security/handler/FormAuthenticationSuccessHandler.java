b3f4ebda5fe500e182ae580f4223d3b8022820df
onjsdnjs <onjsdnjs@gmail.com>
2024-01-29 18:44:42 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/security/handler/FormAuthenticationSuccessHandler.java
+++ src/main/java/io/security/springsecuritymaster/security/handler/FormAuthenticationSuccessHandler.java
@@ -1,18 +1,36 @@
 package io.security.springsecuritymaster.security.handler;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class FormAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+public class FormAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final RequestCache requestCache = new HttpSessionRequestCache();
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
+    @Override
+    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
+
+        setDefaultTargetUrl("/");
+
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        if(savedRequest!=null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+        }
+        else {
+            redirectStrategy.sendRedirect(request, response, getDefaultTargetUrl());
+        }
     }
 }
