37026090d4404636a1bc64e13aa0bc44519556b7
onjsdnjs <onjsdnjs@gmail.com>
2024-01-29 14:55:15 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,17 @@
+package io.security.springsecuritymaster.security.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
