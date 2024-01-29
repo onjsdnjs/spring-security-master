a401ddc9d01be9983d9b4b4fc3beafd8577efe4e
onjsdnjs <onjsdnjs@gmail.com>
2024-01-29 14:03:42 +0900

    프로젝트 생성 및 기본구성

---
+++ src/main/java/io/security/springsecuritymaster/security/service/CustomUserDetailsService.java
@@ -0,0 +1,12 @@
+package io.security.springsecuritymaster.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
