bb5e302269280de6bc17267f99e5fa96ced53181
onjsdnjs <onjsdnjs@gmail.com>
2024-01-30 21:28:25 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/RestApiController.java
+++ src/main/java/io/security/springsecuritymaster/users/RestApiController.java
@@ -1,15 +1,19 @@
 package io.security.springsecuritymaster.users;
 
 import io.security.springsecuritymaster.domain.dto.AccountDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class RestLoginController {
+public class RestApiController {
     @GetMapping(value="/user")
     public AccountDto restUser(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
