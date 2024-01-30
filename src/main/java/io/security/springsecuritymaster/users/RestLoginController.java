0a49f027159e906c1b448ccf571825946b6c8ca9
onjsdnjs <onjsdnjs@gmail.com>
2024-01-30 20:57:07 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/RestLoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/RestLoginController.java
@@ -8,18 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class RestLoginController {
-    @GetMapping(value="/api/user")
+    @GetMapping(value="/user")
     public AccountDto restUser(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
     }
 
-    @GetMapping(value="/api/manager")
+    @GetMapping(value="/manager")
     public AccountDto restManager(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
     }
 
-    @GetMapping(value="/api/admin")
+    @GetMapping(value="/admin")
     public AccountDto restAdmin(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
     }
