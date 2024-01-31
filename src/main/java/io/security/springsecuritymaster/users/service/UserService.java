b9e41af8d287c9074c45ed927ee314847a67b743
onjsdnjs <onjsdnjs@gmail.com>
2024-01-31 10:46:15 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/service/UserService.java
+++ src/main/java/io/security/springsecuritymaster/users/service/UserService.java
@@ -1,7 +1,6 @@
 package io.security.springsecuritymaster.users.service;
 
 import io.security.springsecuritymaster.domain.entity.Account;
-import io.security.springsecuritymaster.repository.UserRepository;
 import io.security.springsecuritymaster.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
