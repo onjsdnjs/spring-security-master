36f4ff9c5bfcc6eb63d47af98ff08b4ef2dca1e4
onjsdnjs <onjsdnjs@gmail.com>
2024-01-31 10:31:21 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/controller/RestLoginController.java
+++ src/main/java/io/security/springsecuritymaster/users/controller/RestLoginController.java
@@ -1,4 +1,4 @@
-package io.security.springsecuritymaster.users;
+package io.security.springsecuritymaster.users.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
