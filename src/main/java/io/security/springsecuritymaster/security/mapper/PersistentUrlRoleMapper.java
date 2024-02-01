12c1556f3e3cdec20ba6612b78a227ef4eafd1e4
onjsdnjs <onjsdnjs@gmail.com>
2024-02-01 17:23:13 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/security/mapper/PersistentUrlRoleMapper.java
+++ src/main/java/io/security/springsecuritymaster/security/mapper/PersistentUrlRoleMapper.java
@@ -1,10 +1,28 @@
 package io.security.springsecuritymaster.security.mapper;
 
+import io.security.springsecuritymaster.admin.repository.ResourcesRepository;
+import io.security.springsecuritymaster.domain.entity.Resources;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PersistentUrlRoleMapper implements UrlRoleMapper{
+
+    private final LinkedHashMap<String, String> urlRoleMappings = new LinkedHashMap<>();
+    private final ResourcesRepository resourcesRepository;
+
+    public PersistentUrlRoleMapper(ResourcesRepository resourcesRepository) {
+        this.resourcesRepository = resourcesRepository;
+    }
     @Override
     public Map<String, String> getUrlRoleMappings() {
-        return null;
+
+        List<Resources> resourcesList = resourcesRepository.findAllResources();
+        resourcesList.forEach(re -> {
+            re.getRoleSet().forEach(role -> {
+                urlRoleMappings.put(re.getResourceName(), role.getRoleName());
+            });
+        });
+        return urlRoleMappings;
     }
 }
