82d63aba6b0fad9f1177dc169f7fcfd322dc2b72
onjsdnjs <onjsdnjs@gmail.com>
2024-02-02 12:20:34 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/security/listener/SetupDataLoader.java
+++ src/main/java/io/security/springsecuritymaster/security/listener/SetupDataLoader.java
@@ -1,12 +1,8 @@
 package io.security.springsecuritymaster.security.listener;
 
-import io.security.springsecuritymaster.admin.repository.ResourcesRepository;
-import io.security.springsecuritymaster.admin.repository.RoleHierarchyRepository;
 import io.security.springsecuritymaster.admin.repository.RoleRepository;
 import io.security.springsecuritymaster.domain.entity.Account;
-import io.security.springsecuritymaster.domain.entity.Resources;
 import io.security.springsecuritymaster.domain.entity.Role;
-import io.security.springsecuritymaster.domain.entity.RoleHierarchy;
 import io.security.springsecuritymaster.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
@@ -14,9 +10,9 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
@@ -24,33 +20,22 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     private boolean alreadySetup = false;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final ResourcesRepository resourcesRepository;
     private final PasswordEncoder passwordEncoder;
-    private final RoleHierarchyRepository roleHierarchyRepository;
-    private static final AtomicInteger count = new AtomicInteger(0);
-
     @Override
     @Transactional
     public void onApplicationEvent(final ContextRefreshedEvent event) {
         if (alreadySetup) {
             return;
         }
-        setupSecurityResources();
+        setupData();
         alreadySetup = true;
     }
 
-    private void setupSecurityResources() {
+    private void setupData() {
         HashSet<Role> roles = new HashSet<>();
-
         Role adminRole = createRoleIfNotFound("ROLE_ADMIN", "관리자");
-//        roles.add(adminRole);
-
-//        createUserIfNotFound("admin", "admin@admin.com", "pass", roles);
-        Role managerRole = createRoleIfNotFound("ROLE_MANAGER", "매니저권한");
-        Role userRole = createRoleIfNotFound("ROLE_USER", "사용자권한");
-
-        createRoleHierarchyIfNotFound(managerRole, adminRole);
-        createRoleHierarchyIfNotFound(userRole, managerRole);
+        roles.add(adminRole);
+        createUserIfNotFound("admin", "admin@admin.com", "pass", roles);
     }
 
     public Role createRoleIfNotFound(String roleName, String roleDesc) {
@@ -62,11 +47,10 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
                     .roleDesc(roleDesc)
                     .build();
         }
-
         return roleRepository.save(role);
     }
 
-    public Account createUserIfNotFound(final String userName, final String email, final String password, Set<Role> roleSet) {
+    public void createUserIfNotFound(final String userName, final String email, final String password, Set<Role> roleSet) {
         Account account = userRepository.findByUsername(userName);
 
         if (account == null) {
@@ -76,45 +60,6 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
                     .userRoles(roleSet)
                     .build();
         }
-
-        return userRepository.save(account);
-    }
-
-    public Resources createResourceIfNotFound(String resourceName, String httpMethod, Set<Role> roleSet, String resourceType) {
-        Resources resources = resourcesRepository.findByResourceNameAndHttpMethod(resourceName, httpMethod);
-
-        if (resources == null) {
-            resources = Resources.builder()
-                    .resourceName(resourceName)
-                    .roleSet(roleSet)
-                    .httpMethod(httpMethod)
-                    .resourceType(resourceType)
-                    .orderNum(count.incrementAndGet())
-                    .build();
-        }
-
-        return resourcesRepository.save(resources);
-    }
-
-    public void createRoleHierarchyIfNotFound(Role childRole, Role parentRole) {
-        RoleHierarchy roleHierarchy = roleHierarchyRepository.findByChildName(parentRole.getRoleName());
-
-        if (roleHierarchy == null) {
-            roleHierarchy = RoleHierarchy.builder()
-                    .childName(parentRole.getRoleName())
-                    .build();
-        }
-        RoleHierarchy parentRoleHierarchy = roleHierarchyRepository.save(roleHierarchy);
-
-        roleHierarchy = roleHierarchyRepository.findByChildName(childRole.getRoleName());
-
-        if (roleHierarchy == null) {
-            roleHierarchy = RoleHierarchy.builder()
-                    .childName(childRole.getRoleName())
-                    .build();
-        }
-
-        RoleHierarchy childRoleHierarchy = roleHierarchyRepository.save(roleHierarchy);
-        childRoleHierarchy.setParentName(parentRoleHierarchy);
+        userRepository.save(account);
     }
 }
