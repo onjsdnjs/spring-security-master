54e760af2a5e42b0846997174c2be69d8114e8a3
onjsdnjs <onjsdnjs@gmail.com>
2024-02-02 13:09:22 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/security/listener/SetupDataLoader.java
@@ -0,0 +1,65 @@
+package io.security.springsecuritymaster.security.listener;
+
+import io.security.springsecuritymaster.admin.repository.RoleRepository;
+import io.security.springsecuritymaster.domain.entity.Account;
+import io.security.springsecuritymaster.domain.entity.Role;
+import io.security.springsecuritymaster.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+    private boolean alreadySetup = false;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    @Transactional
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        if (alreadySetup) {
+            return;
+        }
+        setupData();
+        alreadySetup = true;
+    }
+
+    private void setupData() {
+        HashSet<Role> roles = new HashSet<>();
+        Role adminRole = createRoleIfNotFound("ROLE_ADMINN", "관리자2");
+        roles.add(adminRole);
+        createUserIfNotFound("admin2", "admin@admin.com", "pass", roles);
+    }
+
+    public Role createRoleIfNotFound(String roleName, String roleDesc) {
+        Role role = roleRepository.findByRoleName(roleName);
+
+        if (role == null) {
+            role = Role.builder()
+                    .roleName(roleName)
+                    .roleDesc(roleDesc)
+                    .build();
+        }
+        return roleRepository.save(role);
+    }
+
+    public void createUserIfNotFound(final String userName, final String email, final String password, Set<Role> roleSet) {
+        Account account = userRepository.findByUsername(userName);
+
+        if (account == null) {
+            account = Account.builder()
+                    .username(userName)
+                    .password(passwordEncoder.encode(password))
+                    .userRoles(roleSet)
+                    .build();
+        }
+        userRepository.save(account);
+    }
+}
