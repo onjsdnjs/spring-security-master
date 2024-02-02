9cbcd35a83f2b195265f39c391ca7e8c42c07780
onjsdnjs <onjsdnjs@gmail.com>
2024-02-02 17:26:24 +0900

    spring security master

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,3 +1,5 @@
 insert into role_hierarchy (id, role_name, parent_id) values (1,'ROLE_ADMIN',null);
 insert into role_hierarchy (id, role_name, parent_id) values (2,'ROLE_MANAGER','1');
-insert into role_hierarchy (id, role_name, parent_id) values (3,'ROLE_USER','2');
+insert into role_hierarchy (id, role_name, parent_id) values (3,'ROLE_DBA','1');
+insert into role_hierarchy (id, role_name, parent_id) values (4,'ROLE_USER','2');
+insert into role_hierarchy (id, role_name, parent_id) values (5,'ROLE_USER','3');
