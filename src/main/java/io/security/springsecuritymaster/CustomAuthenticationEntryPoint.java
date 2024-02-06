6af010fb5246416690b80381cb6aa3c631c0a69f
onjsdnjs <onjsdnjs@gmail.com>
2024-02-06 11:43:50 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package io.security.springsecuritymaster;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=localhost");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }
+}
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 import java.io.IOException;
 
@@ -24,22 +25,8 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
-                .formLogin( form -> form
-//                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/")
-                        .failureUrl("/login")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        .successHandler((request, response, authentication) -> {
-                            System.out.println("authentication: " + authentication.getName());
-                            response.sendRedirect("/");
-                        })
-                        .failureHandler((request, response, exception) -> {
-                            System.out.println("exception: " + exception.getMessage());
-                            response.sendRedirect("/login");
-                        })
-                        .permitAll()
+                .httpBasic(basic -> basic
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 );
 
         return http.build();
