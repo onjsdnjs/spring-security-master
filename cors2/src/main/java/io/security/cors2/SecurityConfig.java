9a0f1af83784f4b9bf623e47a8b1bc75a5069463
onjsdnjs <onjsdnjs@gmail.com>
2024-02-06 23:30:04 +0900

    spring security master

--- cors2/src/main/java/io/security/cors2/SecurityConfig.java
+++ cors2/src/main/java/io/security/cors2/SecurityConfig.java
@@ -1,7 +1,7 @@
 package io.security.cors2;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,11 +10,12 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-        http.cors().configurationSource(corsConfigurationSource());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         ;
         return http.build();
     }
