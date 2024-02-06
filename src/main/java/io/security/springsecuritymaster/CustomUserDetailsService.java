3dba13cc917ee72da1002de542d6f86d1730cc46
onjsdnjs <onjsdnjs@gmail.com>
2024-02-06 17:11:55 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/CustomUserDetailsService.java
+++ src/main/java/io/security/springsecuritymaster/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
