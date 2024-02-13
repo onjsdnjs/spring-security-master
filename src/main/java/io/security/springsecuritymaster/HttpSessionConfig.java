a16cab0939995ddc74b7c66230525b353f891390
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 01:30:32 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/HttpSessionConfig.java
@@ -0,0 +1,29 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.MapSession;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Configuration
+@EnableSpringHttpSession
+public class HttpSessionConfig {
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setUseSecureCookie(true);
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setSameSite("Lax");
+        return serializer;
+    }
+    @Bean
+    public SessionRepository<MapSession> sessionRepository() {
+        return new MapSessionRepository(new ConcurrentHashMap<>());
+    }
+}
