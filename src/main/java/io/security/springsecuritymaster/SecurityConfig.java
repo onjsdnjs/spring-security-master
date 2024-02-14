6eee2f3da56448fd75325630f31fd6f48cbd09f2
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 15:14:42 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/csrf","/ignoreCsrf","/form","/formCsrf").permitAll()
+                        .requestMatchers("/createCookie","/readCookie").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(Customizer.withDefaults());
