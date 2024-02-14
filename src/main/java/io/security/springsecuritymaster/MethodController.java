8b4d364943d6139441cc0b5d0b10e8176eea2f0b
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 22:28:27 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/MethodController.java
+++ src/main/java/io/security/springsecuritymaster/MethodController.java
@@ -1,9 +1,6 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,38 +11,5 @@ public class MethodController {
         return "index";
     }
 
-    @GetMapping("/admin")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String admin(){
-        return "admin";
-    }
-    @GetMapping("/user")
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
-    public String user(){
-        return "user";
-    }
 
-    @GetMapping("/isAuthenticated")
-    @PreAuthorize("isAuthenticated()")
-    public String isAuthenticated(){
-        return "isAuthenticated";
-    }
-
-    @GetMapping("/user/{id}")
-    @PreAuthorize("#id == authentication.name")
-    public String authentication(@PathVariable(name = "id") String id){
-        return id;
-    }
-
-    @GetMapping("/owner")
-    @PostAuthorize("returnObject.owner == authentication.name")
-    public Account owner(String name){
-        return new Account(name, false);
-    }
-
-    @GetMapping("/isSecure")
-    @PostAuthorize("hasAuthority('ROLE_ADMIN') and returnObject.isSecure")
-    public Account isSecure(String name, String secure){
-        return new Account(name, "Y".equals(secure));
-    }
 }
