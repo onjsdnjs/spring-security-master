aa1d54d448d35b45e800c3ab7717d0dfd40a7676
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 23:25:46 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/Account.java
+++ src/main/java/io/security/springsecuritymaster/Account.java
@@ -2,11 +2,13 @@ package io.security.springsecuritymaster;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Account {
     private String owner;
     private boolean isSecure;
