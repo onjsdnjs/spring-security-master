15c2bc8de94a2f5728bb1f1acb187e67571dcce0
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 21:54:49 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/MethodController.java
@@ -0,0 +1,33 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MethodController {
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String admin(){
+        return "admin";
+    }
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_USER')")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/isAuthenticated")
+    @PreAuthorize("isAuthenticated()")
+    public String isAuthenticated(){
+        return "isAuthenticated";
+    }
+
+    @GetMapping("/user/{id}")
+    @PreAuthorize("#id == authentication.name")
+    public String authentication(@PathVariable(name = "id") String id){
+        return id;
+    }
+}
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -3,8 +3,8 @@ package io.security.springsecuritymaster;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -25,18 +26,6 @@ public class SecurityConfig {
                 .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-
-        http
-                .securityMatchers((matchers) -> matchers.requestMatchers("/api/**", "/oauth/**"))
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll());
 
         return http.build();
     }
