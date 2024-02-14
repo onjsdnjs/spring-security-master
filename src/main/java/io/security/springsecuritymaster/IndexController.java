61a2006b94cdb66bb0149ac8082f23fb0d787c01
onjsdnjs <onjsdnjs@gmail.com>
2024-02-14 19:55:25 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,17 +1,7 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.CookieGenerator;
 
 @RestController
 public class IndexController {
@@ -20,40 +10,4 @@ public class IndexController {
     public String user(){
         return "user";
     }
-
-    @GetMapping("/myPage/points")
-    public String myPage(){
-        return "myPage";
-    }
-
-    @GetMapping("/manager")
-    public String manager(){
-        return "manager";
-    }
-
-    @GetMapping("/admin")
-    public String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/admin/payment")
-    public String adminPayment(){
-        return "adminPayment";
-    }
-
-    @GetMapping("/resource/address_01")
-    public String address_01(){
-        return "address_01";
-    }
-
-    @GetMapping("/resource/address01")
-    public String address01(){
-        return "address01";
-    }
-
-    @PostMapping("/post")
-    public String post(){
-        return "post";
-    }
-
 }
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -3,11 +3,8 @@ package io.security.springsecuritymaster;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,10 +12,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
@@ -32,7 +25,7 @@ public class SecurityConfig {
         WebExpressionAuthorizationManager expressManager = new WebExpressionAuthorizationManager("@customWebSecurity.check(authentication, request)");
         expressManager.setExpressionHandler(expressionHandler);
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/resource/**").access(expressManager));
+                .requestMatchers("/user/**").access(expressManager));
 
         return http.build();
     }
