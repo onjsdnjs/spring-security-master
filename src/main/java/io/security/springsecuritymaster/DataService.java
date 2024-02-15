f801ef4b9ce70d36aec283f84034bde7027fd8ee
onjsdnjs <onjsdnjs@gmail.com>
2024-02-15 21:24:03 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/DataService.java
+++ src/main/java/io/security/springsecuritymaster/DataService.java
@@ -1,15 +1,8 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class DataService {
--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,10 +1,7 @@
 package io.security.springsecuritymaster;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
