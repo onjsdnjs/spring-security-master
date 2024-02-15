79d5b99bc684ff08b6ddef087945eec364409cf9
onjsdnjs <onjsdnjs@gmail.com>
2024-02-15 22:48:43 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/DataService.java
+++ src/main/java/io/security/springsecuritymaster/DataService.java
@@ -1,16 +1,12 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataService {
-    @PreAuthorize(value = "")
     public String getUser() {
         return "user";
     }
-    @PostAuthorize(value = "")
     public Account getOwner(String name) {
         return new Account(name, false);
     }
