a64b1b5820b2548402f3dc7d5c8aad5f57cbe0a2
onjsdnjs <onjsdnjs@gmail.com>
2024-02-15 22:34:47 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/method/MethodSecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/method/MethodSecurityConfig.java
@@ -1,13 +1,13 @@
 package io.security.springsecuritymaster.method;
 
-import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.ComposablePointcut;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
-import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -17,16 +17,16 @@ import static org.springframework.security.authorization.AuthorityAuthorizationM
 @Configuration
 public class MethodSecurityConfig {
 
-    @Bean
+    /*@Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public Advisor protectServicePointcut() {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression("execution(* io.security.springsecuritymaster.DataService.getUser(..))");
         AuthorityAuthorizationManager<MethodInvocation> manager = hasRole("USER");
         return new AuthorizationManagerBeforeMethodInterceptor(pointcut, manager);
-    }
+    }*/
 
-    /*@Bean
+    @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public Advisor protectServicePointcut() {
         return new AuthorizationManagerBeforeMethodInterceptor(createCompositePointcut(), hasRole("USER"));
@@ -34,15 +34,15 @@ public class MethodSecurityConfig {
 
     public Pointcut createCompositePointcut() {
         AspectJExpressionPointcut pointcut1 = new AspectJExpressionPointcut();
-        pointcut1.setExpression("execution(* io.security.MyService.user(..))");
+        pointcut1.setExpression("execution(* io.security.springsecuritymaster.DataService.getUser(..))");
 
         AspectJExpressionPointcut pointcut2 = new AspectJExpressionPointcut();
-        pointcut2.setExpression("execution(* io.security.MyService.display(..))");
+        pointcut2.setExpression("execution(* io.security.springsecuritymaster.DataService.getOwner(..))");
 
         // 두 포인트컷을 조합
-        ComposablePointcut compositePointcut = new ComposablePointcut(pointcut1);
-        compositePointcut.union(pointcut2);
+        ComposablePointcut compositePointcut = new ComposablePointcut((Pointcut) pointcut1);
+        compositePointcut.union((Pointcut) pointcut2);
 
         return compositePointcut;
-    }*/
+    }
 }
