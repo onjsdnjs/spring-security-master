0196ff53a69e585866eb3971867fd3a871492238
onjsdnjs <onjsdnjs@gmail.com>
2024-02-16 00:05:49 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/CustomAuthenticationProvider2.java
@@ -0,0 +1,36 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomAuthenticationProvider2 implements AuthenticationProvider {
+
+    private final AuthenticationEventPublisher authenticationEventPublisher;
+
+    public CustomAuthenticationProvider2(AuthenticationEventPublisher authenticationEventPublisher) {
+        this.authenticationEventPublisher = authenticationEventPublisher;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        if(!authentication.getName().equals("user")) {
+            authenticationEventPublisher.publishAuthenticationFailure(new BadCredentialsException("DisabledException"), authentication);
+
+            throw new BadCredentialsException("BadCredentialsException");
+        }
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        return new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
