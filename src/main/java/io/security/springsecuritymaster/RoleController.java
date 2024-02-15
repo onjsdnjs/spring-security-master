3a4bfd9024f1091a49c7622f51711e423cd83a0c
onjsdnjs <onjsdnjs@gmail.com>
2024-02-15 17:53:09 +0900

    spring security master

---
+++ src/main/java/io/security/springsecuritymaster/RoleController.java
@@ -0,0 +1,20 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+public class RoleController {
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
