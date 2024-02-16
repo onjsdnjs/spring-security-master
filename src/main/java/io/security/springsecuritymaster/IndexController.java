3e2fa7014530dfe38b5889dc36abce0968b1d066
onjsdnjs <onjsdnjs@gmail.com>
2024-02-16 17:41:42 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,6 +1,7 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
     @GetMapping("/user")
-    public String user(){
+    public CustomUser user(){
         Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
-        return "user";
+        return authentication == null ? null : (CustomUser)authentication.getPrincipal();
+    }
+    @GetMapping("/user2")
+    public CustomUser user2(@AuthenticationPrincipal CustomUser customUser){
+        return customUser;
     }
     @GetMapping("/db")
     public String db(){
