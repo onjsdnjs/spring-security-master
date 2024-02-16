463e2e6d0808e4040edf78d2e63ebac2e4655565
onjsdnjs <onjsdnjs@gmail.com>
2024-02-16 19:00:00 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/AsyncService.java
+++ src/main/java/io/security/springsecuritymaster/AsyncService.java
@@ -1,6 +1,7 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,6 @@ public class AsyncService {
         SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
         System.out.println("securityContext = " + securityContext);
         System.out.println("Child Thread: " + Thread.currentThread().getName());
+        Authentication authentication = securityContext.getAuthentication();
     }
 }
