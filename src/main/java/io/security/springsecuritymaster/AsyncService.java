de0a8f01b7224d945f5f9ae924fe6fa8da23ef0d
onjsdnjs <onjsdnjs@gmail.com>
2024-02-16 18:58:31 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/AsyncService.java
+++ src/main/java/io/security/springsecuritymaster/AsyncService.java
@@ -1,7 +1,6 @@
 package io.security.springsecuritymaster;
 
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
