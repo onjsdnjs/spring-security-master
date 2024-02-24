4911f901f8a3642af308927508ee8b3f6144cd4a
onjsdnjs <onjsdnjs@gmail.com>
2024-02-24 23:45:24 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,13 +1,7 @@
 package io.security.springsecuritymaster;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Callable;
 
 @RestController
 public class IndexController {
