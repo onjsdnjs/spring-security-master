5fa69378ed81e4d3ba10c39c124e6b6e61ead58d
onjsdnjs <onjsdnjs@gmail.com>
2024-02-28 22:22:57 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,7 +1,5 @@
 package io.security.springsecuritymaster;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,36 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
     @GetMapping("/")
-    public String index(String customParam){
-        if(customParam == null){
-            return "index";
-        }else{
-            return "customPage";
-        }
-    }
+    public String index(){
+        return "index";
 
-    @GetMapping("/loginPage")
-    public String login(){
-        return "loginPage";
-    }
-
-    @GetMapping("/anonymous")
-    public String anonymous(){
-        return "anonymous";
-    }
-
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication){
-
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "null";
-        }
-    }
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context){
-        return context.getAuthentication().getName();
     }
 
     @GetMapping("/logoutSuccess")
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,26 +1,16 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -32,15 +22,11 @@ public class SecurityConfig {
         requestCache.setMatchingRequestParameterName("customParam=y");
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/logoutSuccess").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .successHandler((request, response, authentication) -> {
-                            SavedRequest savedRequest = requestCache.getRequest(request, response);
-                            String redirectUrl = savedRequest.getRedirectUrl();
-                            response.sendRedirect(redirectUrl);
-
-                        }))
-                .requestCache(cache -> cache.requestCache(requestCache));
+                .formLogin(Customizer.withDefaults()
+                )
+        ;
         return http.build();
     }
 
