a7bc0a875aa6fedf490659ee4c6848eda15f9e91
onjsdnjs <onjsdnjs@gmail.com>
2024-03-05 17:08:45 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,5 +1,6 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
     @GetMapping("/")
-    public String index(String customParam){
-        if(customParam == null){
-            return "index";
-        }else{
-            return "customPage";
-        }
+    public Authentication index(Authentication authentication, HttpSession session){
+        return authentication;
     }
+
 
     @GetMapping("/loginPage")
     public String login(){
