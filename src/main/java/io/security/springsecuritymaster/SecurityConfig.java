8a7f880e0f29c6eb30256e4aaacbf0deaf2b314f
onjsdnjs <onjsdnjs@gmail.com>
2024-03-08 18:37:42 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -26,10 +26,13 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/login").permitAll()
+
                 .requestMatchers("/user/{name}")
                 .access(new WebExpressionAuthorizationManager("#name == authentication.name"))
+
                 .requestMatchers("/admin/db")
                 .access(new WebExpressionAuthorizationManager("hasAuthority('ROLE_DB') or hasRole('ADMIN')"))
+
                 .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
 
