52e7fb3a1eca72f4e44861a9b67ef8807359f6c8
onjsdnjs <onjsdnjs@gmail.com>
2024-03-13 23:02:41 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/SpringSecurityMasterApplication.java
+++ src/main/java/io/security/springsecuritymaster/SpringSecurityMasterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringSecurityMasterApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityMasterApplication.class, args);
