594eabac755497a584008b729952b215eb618910
onjsdnjs <onjsdnjs@gmail.com>
2024-03-13 18:40:18 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
     @GetMapping("/user")
     public String user(){
         return "user";
