2774d3c113033735178d35a0e93ba89de4ca5251
onjsdnjs <onjsdnjs@gmail.com>
2024-03-14 16:10:13 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/service/UserService.java
+++ src/main/java/io/security/springsecuritymaster/users/service/UserService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
