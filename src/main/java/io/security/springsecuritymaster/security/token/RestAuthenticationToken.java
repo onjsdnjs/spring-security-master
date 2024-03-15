c1a8abbdb98e388a938303aa9fed421113c038e4
onjsdnjs <onjsdnjs@gmail.com>
2024-03-15 16:16:41 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/security/token/RestAuthenticationToken.java
+++ src/main/java/io/security/springsecuritymaster/security/token/RestAuthenticationToken.java
@@ -10,18 +10,18 @@ public class RestAuthenticationToken extends AbstractAuthenticationToken {
     private final Object principal;
     private final Object credentials;
 
+    public RestAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        super(authorities);
+        this.principal = principal;
+        this.credentials = credentials;
+        setAuthenticated(true);
+    }
+
     public RestAuthenticationToken(Object principal, Object credentials) {
         super(null);
         this.principal = principal;
         this.credentials = credentials;
         setAuthenticated(false);
-    }
-
-    public RestAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(authorities);
-        this.principal = principal;
-        this.credentials = credentials;
-        super.setAuthenticated(true);
     }
 
     @Override
