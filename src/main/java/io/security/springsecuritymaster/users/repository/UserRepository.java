01d207e3cffbb094be1f4956e82e4642f966fbf7
onjsdnjs <onjsdnjs@gmail.com>
2024-03-16 10:46:40 +0900

    spring security master

--- src/main/java/io/security/springsecuritymaster/users/repository/UserRepository.java
+++ src/main/java/io/security/springsecuritymaster/users/repository/UserRepository.java
@@ -4,8 +4,6 @@ import io.security.springsecuritymaster.domain.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Account, Long> {
-
     Account findByUsername(String username);
 
-    int countByUsername(String username);
 }
